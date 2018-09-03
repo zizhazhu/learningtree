c305a78e787da0920243257b0220913f0d8352bf
zizhazhu <zizhazhu@gmail.com>
2018-09-03 16:56:38 +0800

    modify: use context to manage result

--- main.py
+++ main.py
@@ -18,6 +18,65 @@ class Field:
         self.inners = inners
 
 
+class Context:
+    def __init__(self, result, tab):
+        self.result = result
+        self.tab = tab
+        self.end = '\n'
+        self.link = []
+
+    def add(self, line):
+        self.result += self.tab + line + self.end
+
+    def addlink(self, src, des):
+        self.link.append('{} -> {}'.format(src, des))
+
+    def writelink(self):
+        for line in self.link:
+            self.result += self.tab + line + self.end
+
+    def __str__(self):
+        return self.result
+
+
+class PrintContext:
+
+    def __init__(self, context, name=''):
+        self.context = context
+        self.name = name
+
+    def __enter__(self):
+        self.context.tab += '\t'
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.context.tab = self.context.tab[:-1]
+        self.context.add('}')
+
+
+class PrintGraph(PrintContext):
+
+    def __init__(self, context, name='G'):
+        super().__init__(context, name)
+
+    def __enter__(self):
+        self.context.add('digraph G {')
+        super().__enter__()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.context.writelink()
+        super().__exit__(exc_type, exc_val, exc_tb)
+
+
+class PrintSubGraph(PrintContext):
+
+    def __init__(self, context, name):
+        super().__init__(context, name)
+
+    def __enter__(self):
+        self.context.add('subgraph cluster{} {{'.format(self.name))
+        super().__enter__()
+
+
 class Graph:
 
     def __init__(self, fields):
@@ -26,31 +85,26 @@ class Graph:
     def output(self):
         inners = set()
         outters = set()
-        result = ''
+        context = Context(result='', tab='')
         end = ''
-        result += 'digraph G {\n'
-        result += '\tsubgraph cluster_0 {\n'
-        for field in self.fields:
-            result += '\t\t' + field.name + ';\n'
-            for inner in field.inners:
-                inners.add(inner)
-                end += '\t{} -> {};\n'.format(field.name, inner.name)
-        result += '\t}\n'
-        result += '\tsubgraph cluster_1 {\n'
-        for inner in inners:
-            result += '\t\t' + inner.name + ';\n'
-            for extend in inner.extends:
-                outters.add(extend)
-                end += '\t{} -> {};\n'.format(inner.name, extend.name)
-        result += '\t}\n'
-        result += '\tsubgraph cluster_2 {\n'
-        for outter in outters:
-            result += '\t\t' + outter.name + ';\n'
-        result += '\t}\n'
-        result += '\n'
-        result += end
-        result += '}\n'
-        return result
+        with PrintGraph(context):
+            with PrintSubGraph(context, 0):
+                for field in self.fields:
+                    context.add(field.name)
+                    for inner in field.inners:
+                        inners.add(inner)
+                        context.addlink(field.name, inner.name)
+            with PrintSubGraph(context, 1):
+                for inner in inners:
+                    context.add(inner.name)
+                    for extend in inner.extends:
+                        outters.add(extend)
+                        context.addlink(inner.name, extend.name)
+            with PrintSubGraph(context, 2):
+                for outter in outters:
+                    context.add(outter.name)
+            context.add(end)
+        return context
 
 
 def main():
@@ -58,7 +112,7 @@ def main():
     dlt = Inner('dlt', [dlp])
     com = Field('com', [dlt])
     with open('my_tree.gv', 'w') as file:
-        file.write(Graph([com]).output())
+        file.write(str(Graph([com]).output()))
 
 
 if __name__ == '__main__':
